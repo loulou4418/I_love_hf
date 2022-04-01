fa050330110cc4c8399077c439c199c75f3d3e7f
Louison_GOUY <louison.gouy@etu.univ-nantes.fr>
2022-04-01 13:46:34 +0200

    schema latex

--- 2carac/zc_octave.m
+++ 2carac/zc_octave.m
@@ -1,3 +1,11 @@
-S11 = 1+i2;
+Adb = -4.205;
+A = 10^(Adb/20) ;
 
-Zc = 50 * (1+S11) / (1-S11)
+phaseDeg = 0;%164;
+phaseRad = 180 * phaseDeg / pi;
+
+Zo = 50;
+
+S11 = A * exp(i*phaseDeg);
+
+Zc = Zo * sqrt((1-S11) / (1+S11));
